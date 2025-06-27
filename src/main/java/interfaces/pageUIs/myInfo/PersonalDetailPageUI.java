cd0a1a04828cfa4809f3691b60616fc9f2176f5d
HuyenNT0420 <huyenhuyenneu04@gmail.com>
2025-06-27 10:29:07 +0700

    check flow to merge on Dev

--- src/main/java/interfaces/pageUIs/myInfo/PersonalDetailPageUI.java
+++ src/main/java/interfaces/pageUIs/myInfo/PersonalDetailPageUI.java
@@ -1,7 +1,5 @@
 package interfaces.pageUIs.myInfo;
 
-import org.checkerframework.checker.index.qual.PolyUpperBound;
-
 public class PersonalDetailPageUI {
     public static final String mainTitle = "//h6[contains(@class, 'orangehrm-main-title') and text()='Personal Details']";
     public static final String FIRSTNAME_VALUE = "//input[@name='firstName']";
@@ -9,5 +7,4 @@ public class PersonalDetailPageUI {
     public static final String LASTNAME_VALUE = "//input[@name='lastName']";
     public static final String EMPLOYEEID_VALUE = "//label[text()='Employee Id']/parent::div/following-sibling::div/input";
     public static final String SAVE_BUTTON_EMPLOYEE_DETAILS = "//div[contains(@class, 'orangehrm-horizontal-padding')]//button[@type = 'submit']";
-
 }
